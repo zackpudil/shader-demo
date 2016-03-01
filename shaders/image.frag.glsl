448b72677157179fb5f165e05b3959f47fd9abc5
pudilz <zack.pudil@dowjones.com>
2016-02-29 19:38:51 -0600

    Added materials, refactored the way colors are saved for intersection

--- shaders/image.frag.glsl
+++ shaders/image.frag.glsl
@@ -1,4 +1,38 @@
 #define pi 3.1415926535897932384626433832795
+mat3 m = mat3( 0.00,  0.80,  0.60,
+              -0.80,  0.36, -0.48,
+              -0.60, -0.48,  0.64 );
+
+float hash( float n ) {
+  return fract(sin(n)*43758.5453);
+}
+
+
+float noise( in vec3 x ) {
+  vec3 p = floor(x);
+  vec3 f = fract(x);
+
+  f = f*f*(3.0-2.0*f);
+
+  float n = p.x + p.y*57.0 + 113.0*p.z;
+
+  float res = mix(mix(mix( hash(n+  0.0), hash(n+  1.0),f.x),
+                      mix( hash(n+ 57.0), hash(n+ 58.0),f.x),f.y),
+                  mix(mix( hash(n+113.0), hash(n+114.0),f.x),
+                      mix( hash(n+170.0), hash(n+171.0),f.x),f.y),f.z);
+  return res;
+}
+
+float fbm( vec3 p ) {
+  float f = 0.0;
+
+  f += 0.5000*noise( p ); p = m*p*2.02;
+  f += 0.2500*noise( p ); p = m*p*2.03;
+  f += 0.1250*noise( p ); p = m*p*2.01;
+  f += 0.0625*noise( p );
+
+  return f/0.9375;
+}
 
 // distance fields
 float plane(vec3 p, vec4 n) {
@@ -25,18 +59,18 @@ float cylinder(vec3 p, vec2 h) {
 }
 
 // operations on distance fields
-vec4 unionRightAngle(vec4 a, vec4 b) {
+vec2 unionRightAngle(vec2 a, vec2 b) {
   float d = min(a.x, b.x);
-  return vec4(d, d == a.x ? a.yzw : b.yzw);
+  return vec2(d, d == a.x ? a.y : b.y);
 }
 
-vec4 unionChamfer(vec4 a, vec4 b, float r) {
-  return min(min(a, b), (a - vec4(r) + b)*sqrt(0.5));
+vec2 unionChamfer(vec2 a, vec2 b, float r) {
+  return min(min(a, b), (a - vec2(r) + b)*sqrt(0.5));
 }
 
-vec4 compliment(vec4 a, vec4 b) {
+vec2 compliment(vec2 a, vec2 b) {
   float d = max(-a.x, b.x);
-  return vec4(d, d == a.x ? a.yzw : b.yzw);
+  return vec2(d, d == a.x ? a.y : b.y);
 }
 
 // Domain operations
@@ -75,49 +109,83 @@ void rotateX(inout vec3 p, float a) {
   p = rx*p;
 }
 
-vec4 scene(vec3 p) {
-  vec3 color = vec3(0.75);
+vec3 sphereMaterial(vec3 p) {
+  vec3 sphereColor = vec3(1.0,0.0,0.0);
+  float a = atan(p.x,p.z);
+  float r = length(p.xz);
+  float f = smoothstep( 0.1, 1.0, fbm(p) );
+  sphereColor = mix( sphereColor, vec3(0.0,0.0,1.0), f );
+  f = smoothstep( 0.0, 1.0, fbm(p*4.0) );
+  sphereColor *= 0.8+0.2*f;
 
-  vec4 planeDI = vec4(
-    plane(p, vec4(0, 1, 0, 0)),
-    vec3(1.0)*mod(floor(p.z) + floor(p.x), 2.0));
+  f = fbm( vec3(a*7.0 + p.z,3.0*p.y,p.x)*2.0);
+  f = smoothstep( 0.2,1.0,f);
+  f *= smoothstep(0.4,1.2,p.y + 0.75*(noise(4.0*p.zyx)-0.5) );
+  sphereColor = mix( sphereColor, vec3(0.4,0.2,0.0), 0.5*f );
 
+  return sphereColor;
+}
+
+vec3 buildingMaterial(vec3 p) {
+  vec3 wallColor = vec3(0.25);
+  float f = fbm( 4.0*p*vec3(1.0,9.0,0.5) );
+  wallColor = mix( wallColor, vec3(0.2,0.2,0.2)*1.7, f );
+  f = fbm( 2.0*p);
+  wallColor *= 0.7+0.3*f;
+
+  f = smoothstep(0.0, 1.0, fbm(p*48.0));
+  f = smoothstep(0.7, 0.9, f);
+  wallColor = mix(wallColor, vec3(0.2), f*0.75);
+
+  return wallColor;
+}
+
+vec3 floorMaterial(vec3 p) {
+  vec3 planeColor = vec3(1.0)*mod(floor(p.z) + floor(p.x), 2.0);
+  return planeColor;
+}
+
+vec2 scene(vec3 p) {
+  vec2 planeDI = vec2(plane(p, vec4(0, 1, 0, 0)),0.0);
   p.y -= 1.45;
+
+  rep(p.xz, vec2(13, 13));
   mirrorLeft(p.xz, vec2(5, 5));
-  vec4 sphereDI = vec4(sphere(p + vec3(0, -1.0, 0), 0.6), vec3(1, 0, 0));
+  vec2 sphereDI = vec2(sphere(p + vec3(0, -1.0, 0), 0.6), 1.0);
+
   vec3 m = p;
   mirrorRight(m.xz, vec2(0.15, 0.15));
-  vec4 column = vec4(cylinder(m + vec3(0, 0.62, 0), vec2(0.2, 0.9)), color);
+  vec2 column = vec2(cylinder(m + vec3(0, 0.62, 0), vec2(0.2, 0.9)), 2.0);
   m = p;
   m.y += 0.53;
   mirrorLeft(m.y, 0.82);
-  vec4 platform = vec4(box(m, vec3(0.7, 0.1, 0.7)), color);
+  vec2 platform = vec2(box(m, vec3(0.7, 0.1, 0.7)), 2.0);
 
-  vec4 pillar = unionChamfer(platform, column, 0.1);
+  vec2 pillar = unionChamfer(platform, column, 0.1);
   pillar = unionRightAngle(sphereDI, pillar);
   mirrorRight(p.xz, vec2(5, 5));
   mirrorRight(p.z, 1.5);
-  vec4 wall = vec4(box2(p.yz, vec2(1.4, 0.05)), color);
+  vec2 wall = vec2(box2(p.yz, vec2(1.4, 0.05)), 2.0);
 
   m = p;
   m.yz -= vec2(2.3, 1.05);
   rotateX(m, 45.0);
-  vec4 roof = vec4(box2(m.yz, vec2(0.05, 1.7)), color);
-  vec4 building = unionRightAngle(roof, wall);
+  vec2 roof = vec2(box2(m.yz, vec2(0.05, 1.7)), 2.0);
+  vec2 building = unionRightAngle(roof, wall);
 
   rep(p.x, 1.5);
   mirrorLeft(p.x, 0.55);
   mirrorLeft(p.x, -0.2);
   p.y += 0.3;
-  vec4 boxDI = vec4(box(p, vec3(0.6, 0.7, 0.1)), color);
+  vec2 boxDI = vec2(box(p, vec3(0.6, 0.7, 0.1)), 0.0);
   p.y -= 0.7;
-  vec4 cylinderDI = vec4(cylinder(p.xzy, vec2(0.6, 0.1)), color);
+  vec2 cylinderDI = vec2(cylinder(p.xzy, vec2(0.6, 0.1)), 0.0);
 
-  vec4 windows = unionRightAngle(boxDI, cylinderDI);
+  vec2 windows = unionRightAngle(boxDI, cylinderDI);
   building = compliment(windows, building);
   building = unionRightAngle(building, pillar);
 
-  vec4 scene = unionRightAngle(planeDI, building);
+  vec2 scene = unionRightAngle(planeDI, building);
 
   return scene;
 }
@@ -179,20 +247,20 @@ vec3 getShading(vec3 p, vec3 n, vec3 l) {
   return vec3(1.0)*ints + vec3(0.4)*(1.0 - ints)*(1.0 - ao);
 }
 
-vec4 raymarch(vec3 rayOrigin, vec3 rayDirection) {
+vec2 raymarch(vec3 rayOrigin, vec3 rayDirection) {
   float t = 0.0;
-  vec3 m = vec3(-1.0);
+  float m = -1.0;
 
   for(int i = 0; i < 64; i++) {
     vec3 p = rayOrigin + rayDirection*t;
-    vec4 s = scene(p);
+    vec2 s = scene(p);
     if(s.x < 0.0002) break;
     t += s.x;
-    m = s.yzw;
+    m = s.y;
   }
 
-  if(t > 50.0) m = vec3(-1.0);
-  return vec4(t, m);
+  if(t > 50.0) m = -1.0;
+  return vec2(t, m);
 }
 
 void tryImage(out vec4 fragColor, in vec2 fragCoord) {
@@ -204,15 +272,22 @@ void tryImage(out vec4 fragColor, in vec2 fragCoord) {
   int i;
   float t;
 
-  vec4 render = raymarch(ro, rd);
+  vec2 render = raymarch(ro, rd);
 
   if(render.y > -1.0) {
     vec3 pos = ro + render.x*rd;
     vec3 normal = getNormal(pos);
-    vec3 shading =
-      getShading(pos, normal, vec3(0, 10, 0));
+    vec3 shading = getShading(pos, normal, vec3(0, 10, 0));
 
-    fragColor = vec4(pow(render.yzw*shading, vec3(0.474)), 1);
+    vec3 col;
+    if(render.y == 0.0)
+      col = floorMaterial(pos);
+    else if(render.y == 1.0)
+      col = sphereMaterial(pos);
+    else if(render.y == 2.0)
+      col = buildingMaterial(pos);
+
+    fragColor = vec4(pow(col*shading, vec3(0.474)), 1);
   } else {
     fragColor = vec4(vec3(0.25), 1.0);
   }
